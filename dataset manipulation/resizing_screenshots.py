fd28f3a9e706463a9dad929ce4771bd280dd6893
KarimIbrahim11 <karim.ibrahim.amin@gmail.com>
2021-06-23 04:09:15 +0200

    Resizing screenshots maintaining aspect ration (courtesy of tifa)

---
+++ dataset manipulation/resizing_screenshots.py
@@ -0,0 +1,32 @@
+import os
+
+from PIL import Image
+
+path = 'D:/College/Semester 9/GP/Codes/Datasets/Custom Dataset/Foul/Screenshots/'
+newpath = 'D:/College/Semester 9/GP/Codes/Datasets/Custom Dataset/Foul/Screenshots2/'
+
+
+def resize():
+    c = "Foul"
+    i = 1
+    for item in os.listdir(path):
+        if os.path.isfile(path + item):
+            im = Image.open(path + item)
+            im = im.convert('RGB')
+            w, h = im.size
+            if w > 950 and h > 950:
+                w = w // 3
+                h = h // 3
+                im = im.resize((w, h), Image.ANTIALIAS)
+            elif w > 600 and h > 600:
+                w = w // 2
+                h = h // 2
+                im = im.resize((w, h), Image.ANTIALIAS)
+            im.save(newpath + c + str(i) + '.jpg')
+            i += 1
+
+            # imResize = im.resize((200,200), Image.ANTIALIAS)
+            # imResize.save(f + ' resized.jpg', 'JPEG', quality=90)
+
+
+resize()
